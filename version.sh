d6a3f4459adbd9dbbd67998bc292409f68c3f460
Mondeep1999 <90750345+Mondeep1999@users.noreply.github.com>
2023-11-06 14:33:43 +0530

    Update version.sh

--- version.sh
+++ version.sh
@@ -15,11 +15,11 @@ else
     last_jar=$( cat version.txt )
 
     # echo $last_jar                   # jar-v1.0.5
-    i=$(echo "$last_jar" | awk -F'[-.]' '{print $2}'| sed 's/[^0-9]*//g')
+    i=$(echo "$last_jar" | awk -F'[-.]' '{print $1}'| sed 's/[^0-9]*//g')
     # echo $i
-    j=$(echo "$last_jar" | awk -F'[-.]' '{print $3}'| sed 's/[^0-9]*//g')
+    j=$(echo "$last_jar" | awk -F'[-.]' '{print $2}'| sed 's/[^0-9]*//g')
     # echo $j
-    k=$(echo "$last_jar" | awk -F'[-.]' '{print $4}'| sed 's/[^0-9]*//g')
+    k=$(echo "$last_jar" | awk -F'[-.]' '{print $3}'| sed 's/[^0-9]*//g')
     # echo $k
     # name="$i$j$k"
 
@@ -41,3 +41,10 @@ fi
 
 cp -r /home/runner/work/java_versioning/java_versioning/app.release.txt .
 mv app.release.txt app-v$i.$j.$k.txt
+ls -al
+git add .
+git commit -m "version update $i.$j.$k"
+git tag -a v$i.$j.$k -m "version $i.$j.$k"
+git tag 
+git push https://${{ secrets.username }}:${{ secrets.pass }}@${{ secrets.git_url }}.git $i.$j.$k
+git push https://${{ secrets.username }}:${{ secrets.pass }}@${{ secrets.git_url }}.git target
